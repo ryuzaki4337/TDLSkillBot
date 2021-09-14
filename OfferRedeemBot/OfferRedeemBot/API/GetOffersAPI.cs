d162722e32afcc6844134fb5325152b62288ddcb
Lalithaditya Kota <lalithaditya.kota@tatadigital.com>
2021-09-14 16:43:50 +0530

    Creating a use case for the TCP bot

---
+++ OfferRedeemBot/OfferRedeemBot/API/GetOffersAPI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace  OfferRedeemBot.API
+{
+    public static class GetOffersAPI
+    {
+        private readonly string baseUrl = "....";
+
+        public static List<string> GetOffersCustomerId(string customerId)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(baseUrl + "/" + customerId);
+            webRequest.Proxy.Credentials = CredentialCache.DefaultCredentials;
+            webRequest.UserAgent = "Awesome-Octocat-App";
+            webRequest.UseDefaultCredentials = true;
+            webRequest.Method = "GET";
+            HttpWebResponse httpWebResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            var offersList = new List<string>();
+
+            string resJSON = "";
+            using (Stream stream = httpWebResponse.GetResponseStream())
+            {
+                StreamReader streamReader = new StreamReader(stream);
+                resJSON = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+
+            JToken jToken = JToken.Parse(resJSON);
+
+            var jsonArrayLength = ((Newtonsoft.Json.Linq.JArray)jToken).Count;
+
+            for (int i = 0; i < jsonArrayLength; i++)
+            {
+                offersList.Add(jToken[i]["offerName"]);
+            }
+
+            return offersList;
+        }
+        
+    }
+
+
+}
